5e2ece5c261351218f3262ad44b3c6882c6cfab5
Vadoniy <kurbatov_1989@inbox.ru>
2021-11-26 20:55:15 +0300

    hw-18 1st commit

--- src/main/java/ru/otus/yardsportsteamlobby/YardSportLeagueApplication.java
+++ src/main/java/ru/otus/yardsportsteamlobby/YardSportLeagueApplication.java
@@ -2,12 +2,14 @@ package ru.otus.yardsportsteamlobby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableSwagger2
 public class YardSportLeagueApplication {
 
--- src/main/java/ru/otus/yardsportsteamlobby/rest/GameRestController.java
+++ src/main/java/ru/otus/yardsportsteamlobby/rest/GameRestController.java
@@ -1,7 +1,9 @@
 package ru.otus.yardsportsteamlobby.rest;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.yardsportsteamlobby.configuration.BusinessConfiguration;
@@ -12,7 +14,10 @@ import ru.otus.yardsportsteamlobby.rest.request.game.CreateGameRequest;
 import ru.otus.yardsportsteamlobby.rest.response.game.ListGameResponse;
 import ru.otus.yardsportsteamlobby.service.GameService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +28,18 @@ public class GameRestController {
 
     private final GameService gameService;
 
+    private List<GameDto> cachedGameList = new ArrayList<>(0);
+
+    private GameDto cachedGameDto = new GameDto();
+
     @PostMapping("/game/new")
-    public String createGame(@RequestBody CreateGameRequest createGameRequest) {
+    @HystrixCommand(commandKey = "gameServiceTimeout", defaultFallback = "gameNotCreated")
+    public ResponseEntity<String> createGame(@RequestBody CreateGameRequest createGameRequest) throws InterruptedException {
+        final var tt = new Random().nextInt(10);
+        if (tt%2==0) {
+            log.info("COUNTER {}", new Random().nextInt(10));
+            Thread.sleep(1500);
+        }
         final var teamNameA = Optional.ofNullable(createGameRequest.getTeamNameA())
                 .orElse(businessConfiguration.getTeamNameA());
         final var teamNameB = Optional.ofNullable(createGameRequest.getTeamNameB())
@@ -39,17 +54,38 @@ public class GameRestController {
                 .setTeamA(teamA)
                 .setTeamB(teamB);
         final var gameSaved = gameService.saveGame(game);
-        return "Game is created! Date " + gameSaved.getGameDateTime().toLocalDate() + ", time " + gameSaved.getGameDateTime().toLocalTime()
+        final var response = "Game is created! Date " + gameSaved.getGameDateTime().toLocalDate() + ", time " + gameSaved.getGameDateTime().toLocalTime()
                 + ", teams: " + gameSaved.getTeamA().getTeamName() + " - " + gameSaved.getTeamB().getTeamName();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/game/list/{amountOfGames}")
+    @HystrixCommand(commandKey = "gameServiceTimeout", defaultFallback = "cachedGamesList")
     public ListGameResponse getGameList(@PathVariable int amountOfGames) {
+        cachedGameList = Optional.ofNullable(gameService.gameList(amountOfGames))
+                .map(ListGameResponse::getGames)
+                .orElse(new ArrayList<>(0));
         return gameService.gameList(amountOfGames);
     }
 
     @PostMapping("/game/{gameId}/team/{teamId}/player/{userId}")
+    @HystrixCommand(commandKey = "gameServiceTimeout", defaultFallback = "cachedGameDto")
     public ResponseEntity<GameDto> signUpForGame(@PathVariable long gameId, @PathVariable long teamId, @PathVariable long userId) {
         return gameService.signUpForGame(gameId, teamId, userId);
+    }
+
+    private ResponseEntity<String> gameNotCreated() {
+        final var response = "Game was not created, try again later.";
+        return new ResponseEntity<>(response, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+    }
+
+    private ListGameResponse cachedGamesList() {
+        final var response = new ListGameResponse();
+        response.setGames(cachedGameList);
+        return response;
+    }
+
+    private ResponseEntity<GameDto> cachedGameDto() {
+        return new ResponseEntity<>(cachedGameDto, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
     }
 }
